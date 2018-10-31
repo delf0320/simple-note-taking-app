f704fcf2120a4fcf1f70a31d4fe24c254f88a4f0
Delf Carl Boston <delf.boston@gmail.com>
2018-10-31 17:54:37 +0800

    Add edit and update on note

---
+++ simple-note-taking-app-master/app/src/main/java/com/notes/boston/simplenotes/MainActivity.java
@@ -0,0 +1,128 @@
+package com.notes.boston.simplenotes;
+
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.database.sqlite.SQLiteDatabase;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity implements AdapterView.OnItemClickListener {
+
+    private DbHelper dbHelper = null;
+    private SQLiteDatabase db = null;
+    private ArrayList<Note> notes = new ArrayList<>();
+    private ListView notesList = null;
+    private NotesAdapter adapter = null;
+    private Intent noteIntent;
+
+    public static final String TAG = "MainActivity";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        noteIntent = new Intent(this, NoteEditor.class);
+
+        dbHelper = new DbHelper(this);
+        db = dbHelper.getWritableDatabase();
+
+        notesList = findViewById(R.id.notes_list);
+
+        notesList.setOnItemClickListener(this);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        notes =  dbHelper.getAllNotes(db);
+        adapter = new NotesAdapter(this, R.layout.notes_item, notes);
+        notesList.setAdapter(adapter);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.mainmenu, menu);
+
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        if(id == R.id.addNote){
+            // Start the add note activity here
+            Intent noteIntent = new Intent(this, NoteEditor.class);
+            startActivity(noteIntent);
+        }
+
+        return true;
+    }
+
+    @Override
+    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+        final Note selectedNote = notes.get(position);
+        final long noteID = selectedNote.getId();
+
+        // We need to show the dialogue
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setMessage(selectedNote.getTitle() + "\n\n" +selectedNote.getBody());
+        builder.setCancelable(false);
+        builder.setPositiveButton("Edit", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                // Launch the notes activity
+                noteIntent.putExtra("noteid", noteID);
+                noteIntent.putExtra("Title",selectedNote.getTitle());
+                noteIntent.putExtra("Body",selectedNote.getBody());
+                startActivity(noteIntent);
+            }
+        });
+
+        builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) { }
+        });
+
+        builder.setNeutralButton("Delete", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                // perform delete
+                boolean isDeleteSuccessful = dbHelper.deleteNote(db, noteID);
+                if(isDeleteSuccessful){
+                    Toast.makeText(MainActivity.this, "Note is deleted!", Toast.LENGTH_LONG).show();
+
+                    notes.remove(selectedNote);
+                    adapter.setDataset(notes);
+                    adapter.notifyDataSetChanged();
+                }
+            }
+        });
+
+        builder.show();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+
+        db.close();
+    }
+
+    @Override
+    public void onPointerCaptureChanged(boolean hasCapture) {
+
+    }
+}
---
+++ simple-note-taking-app-master/app/src/main/java/com/notes/boston/simplenotes/NoteEditor.java
@@ -0,0 +1,76 @@
+package com.notes.boston.simplenotes;
+
+import android.content.Intent;
+import android.database.sqlite.SQLiteDatabase;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class NoteEditor extends AppCompatActivity {
+
+    private DbHelper dbHelper = null;
+    private SQLiteDatabase db = null;
+
+    public EditText conTitle;
+    public EditText conBody;
+    private Intent intent;
+    private String title;
+    private String body;
+    private long col_ID;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_note_editor);
+
+        intent = getIntent();
+
+        dbHelper = new DbHelper(this);
+        db = dbHelper.getWritableDatabase();
+
+        conTitle = findViewById(R.id.note_title);
+        conBody = findViewById(R.id.note_body);
+
+        title = intent.getStringExtra("Title");
+        body = intent.getStringExtra("Body");
+        col_ID = intent.getLongExtra("noteid", 1);
+
+        conTitle.setText(title);
+        conBody.setText(body);
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.note_menu, menu);
+
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        if(id == R.id.itemSave){
+            if(col_ID == 1) {
+                long add = dbHelper.insertNote(db, conTitle.getText().toString(), conBody.getText().toString());
+
+                Toast.makeText(this, "Note has been saved!", Toast.LENGTH_SHORT).show();
+
+                this.finish();
+            }
+            else
+            {
+                boolean update = dbHelper.updateNote(db, col_ID ,conTitle.getText().toString(), conBody.getText().toString());
+                Toast.makeText(this, "Note has been updated!", Toast.LENGTH_SHORT).show();
+
+                this.finish();
+            }
+        }
+
+        return true;
+    }
+}
